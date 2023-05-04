ee093f1a8f8977b7286176c34831b8d7c99060e6
YingJames <jamesy148@usf.edu>
2023-05-04 15:00:26 -0400

    add functionality to contact form

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,55 +1,75 @@
+import React from 'react';
+import { Alert } from 'flowbite-react';
+import { useForm, ValidationError } from '@formspree/react';
 import { Label, TextInput, Textarea, Button } from 'flowbite-react';
 import { HiMail, HiUser } from 'react-icons/hi';
 
 export default function ContactForm() {
+    const [state, handleSubmit] = useForm("xeqwnrja");
+
+    function SuccessAlert() {
+        return (
+            <Alert className="my-3" color="success" onDismiss={function onDismiss() { return <ContactForm /> }}
+            >
+                <span>
+                    <span className="font-bold">
+                        Message sent!
+                    </span>
+                    {' '}Thank you for messaging. I will get back to you as soon as possible.
+                </span>
+            </Alert>
+        )
+    }
+    
+    if (state.succeeded) {
+        return <SuccessAlert />
+    }
+
     return (
-        <>
-            <form className="flex flex-col gap-4">
-                <div>
-                <div>
-                    <div className="mb-2 block">
-                        <Label
-                            htmlFor="name"
-                            value="Name"
-                        />
-                    </div>
-                    <TextInput
-                        className="mb-2"
-                        id="name"
-                        icon={HiUser}
-                        placeholder="John Doe"
-                        required={true}
-                    />
-                </div>
-                    <div className="mb-2 block">
-                        <Label
-                            htmlFor="email"
-                            value="Email"
-                        />
-                    </div>
-                    <TextInput
-                        id="email"
-                        type="email"
-                        icon={HiMail}
-                        placeholder="name@email.com"
-                        required={true}
-                    />
-                </div>
-                <div id="textarea">
-                <div className="mb-2 block">
-                    <Label
-                    htmlFor="message"
-                    value="Message"
-                    />
-                </div>
+        <>  
+            <form className="flex flex-col gap-4 my-3" onSubmit={handleSubmit}>
+                <Label htmlFor="name">Name</Label>
+                <TextInput
+                    id="name"
+                    type="text"
+                    name="name"
+                    icon={HiUser}
+                    placeholder="First and Last name"
+                    required={true}
+                />
+
+                <Label htmlFor="email">
+                    Email Address
+                </Label>
+                <TextInput
+                    id="email"
+                    type="email"
+                    name="email"
+                    icon={HiMail}
+                    placeholder="name@email.com"
+                    required={true}
+                />
+                <ValidationError
+                    prefix="Email"
+                    field="email"
+                    errors={state.errors}
+                />
+
+                <Label htmlFor="message">Message</Label>
                 <Textarea
                     id="message"
-                    placeholder="Message"
+                    name="message"
+                    placeholder="Your message here..."
                     required={true}
-                    rows={4}
+                    rows={3}
                 />
-                </div>
-                <Button type="submit">
+                <ValidationError
+                    prefix="Message"
+                    field="message"
+                    errors={state.errors}
+                />
+
+                <Button type="submit" disabled={state.submitting}>
                     Submit
                 </Button>
             </form>
