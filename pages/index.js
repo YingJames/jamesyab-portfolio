c4999b60f7073dec858c328250bea32362fa9c89
jamesy <jyab0617@gmail.com>
2024-05-08 00:20:15 -0400

    remove unused imports

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import Link from 'next/link';
 import styles from '@/styles/Home.module.css';
 import Navigation from '@/components/Navigation';
-import { Button } from 'flowbite-react';
 import Footer from '@/components/Footer';
 
 import HeroSection from '@/components/HomeSections/HeroSection';
